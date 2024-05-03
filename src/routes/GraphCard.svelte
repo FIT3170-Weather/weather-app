bf2f3f3ced5c606eb0434a8637c0a26d3e2d89b9
ryanCYJ <rcho0046@student.monash.edu>
2024-05-03 08:09:04 +0800

    Created graph card

---
+++ src/routes/GraphCard.svelte
@@ -0,0 +1,9 @@
+
+
+<div class="border border-white card bg-gradient-to-b from-indigo-500 to-indigo-400 shadow-xl">
+    <div class="card-body items-center">
+        <canvas></canvas>
+    </div>
+</div>
+
+
