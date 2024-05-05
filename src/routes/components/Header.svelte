3eacdb8b7339fb9ae2f06481a87ddc51885eb723
lcar0029 <lcar0029@student.monash.edu>
2024-05-05 20:52:41 +0800

    Created website layout and added top navigation bar

---
+++ src/routes/components/Header.svelte
@@ -0,0 +1,13 @@
+<div class="navbar bg-base-200">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl"><img src="../../src/lib/images/climate_text_logo.png" alt="logo" width="100"></a>
+    </div>
+    <div class="flex-none gap-2">
+        <div>
+
+        </div>
+        <div class="form-control">
+          <input type="text" placeholder="Search location, city, postal code, or place" class="search-bar input input-bordered w-200 md:w-auto bg-base-100" />
+        </div>
+    </div>
+  </div>
