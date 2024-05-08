38c21187f79a036438f2de2c1595a8dd3072dab8
ammar-siraj4 <amoh0157@student.monash.edu>
2024-05-08 15:40:28 +0800

    Add basecode for side-nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script>
+	import SideNav from "./components/SideNav.svelte";
 </script>
+
+<SideNav/>
 
 <svelte:head>
 	<title>CliMate</title>
 	<meta name="description" content="Climate web app" />
 </svelte:head>
 
-<section>
+<section class="content">
 	<!-- 3x4 grid used for dashboard -->
 	<!-- 100vh - 64px (header height), so dashboard fills remaining height -->
-	<div class="grid grid-rows-3 grid-cols-4 gap-5 p-10 justify-center h-[calc(100vh-64px)] w-screen">
+	<div class="grid grid-rows-3 grid-cols-4 gap-5 p-10 justify-center h-[calc(100vh-64px)] w-screen grid-content">
 		<!-- TODO: Main location here -->
 		<div class="row-span-2 col-span-2 border h-full w-full grow">Main Location</div>
 		<!-- TODO: Graphs here -->
@@ -20,4 +23,14 @@
 </section>
 
 <style>
+	.content {
+       margin-left: 70px; /* Same width as the side nav */
+       margin-right: 10px;
+       /* Add additional styles as needed */
+   }
+
+   .grid-content {
+       width: 100%;
+   }
+
 </style>
--- src/routes/components/SideNav.svelte
+++ src/routes/components/SideNav.svelte
@@ -1,6 +1,42 @@
-<!-- This doesn't work
-<div class="sidenav-custom w-16 flex h-screen -z-1 items-center justify-center bg-base-200">
-    <h1>Nav</h1>
-</div> -->
-
-<!-- TODO -->
+<script>
+    export let isOpen = true; // Initially open
+  </script>
+   <div class="sidenav" class:open={isOpen}>
+    <div class="button-container">
+        <!-- Add buttons here -->
+    </div>
+  </div>
+   <style>
+    .button-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    }
+   
+    .sidenav {
+      position: fixed;
+      top: 60px;
+      left: 0;
+      bottom: 0%;
+      width: 70px;
+      background-color: #13122F;
+      padding: 10px;
+      transition: transform 0.3s ease;
+      transform: translateX(-100%);
+    }
+     .sidenav.open {
+      transform: translateX(0);
+    }
+     button {
+      display: block;
+      margin-bottom: 10px;
+      background: none;
+      border: none;
+      cursor: pointer;
+    }
+     img {
+      width: 100%;
+    }
+  </style>
+ 
