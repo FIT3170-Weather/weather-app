9348894de744e608c6661cbcee3875a8642bcf0d
Shyam Kamalesh Borkar <sbor0018@student.monash.edu>
2024-05-12 17:53:28 +0800

    Added UI for main weather card, requires some css touches

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	// @ts-ignore
+	import CurrentWeatherCard from "./components/dashboard/CurrentWeatherCard.svelte";
 </script>
 
 <svelte:head>
@@ -11,7 +13,9 @@
 	<!-- 100vh - 64px (header height), so dashboard fills remaining height -->
 	<div class="grid grid-rows-3 grid-cols-4 gap-5 p-10 justify-center h-[calc(100vh-64px)] w-screen">
 		<!-- TODO: Main location here -->
-		<div class="row-span-2 col-span-2 border h-full w-full grow">Main Location</div>
+		<div class="glassmorph row-span-2 col-span-2 border h-full w-full grow">
+			<CurrentWeatherCard />
+		</div>
 		<!-- TODO: Graphs here -->
 		<div class="row-span-2 col-span-2 border h-full w-full grow">Graphs</div>
 		<!-- TODO: Other locations here (bottom row of locations) -->
@@ -20,4 +24,13 @@
 </section>
 
 <style>
+	.glassmorph {
+		/* From https://css.glass */
+		background: rgba(255, 255, 255, 0.11);
+		border-radius: 16px;
+		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+		backdrop-filter: blur(2.9px);
+		-webkit-backdrop-filter: blur(2.9px);
+		border: 1px solid rgba(255, 255, 255, 0.69);
+	}
 </style>
