372dd0be5bb43db84ba03e9b3f97de795045e05a
Suman Datta <sdat0004@student.monash.edu>
2024-05-16 06:20:30 +0800

    Refactored.glassmorph to use TailwindCSS' @apply

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,23 +10,23 @@
 
 <div class="flex m-4">
 	<!-- Current weather -->
-	<div class="glassmorph mr-2 p-2 rounded-2xl">
+	<div class="glassmorph mr-2 p-2">
 		<CurrentWeatherCard />
 	</div>
 
 	<!-- Today's insights -->
-	<div class="grow place-content-center ml-2 glassmorph rounded-2xl">
+	<div class="grow place-content-center ml-2 glassmorph">
 		<p class="text-center">Today's Insights</p>
 	</div>
 </div>
 
 <!-- Other cities -->
-<div class="m-4 mt-0 place-content-center glassmorph rounded-2xl grow">
+<div class="m-4 mt-0 place-content-center glassmorph grow">
 	<p class="text-center">Other Cities</p>
 </div>
 
 <style>
 	.glassmorph {
-		@apply border bg-white bg-opacity-10 shadow-lg;
+		@apply border bg-white bg-opacity-10 shadow-lg rounded-2xl;
 	}
 </style>
