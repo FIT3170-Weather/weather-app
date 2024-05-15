86c0dbbe715fa99ab22af57203aaeb41e542818c
Suman Datta <sdat0004@student.monash.edu>
2024-05-16 06:19:12 +0800

    Added comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,18 @@
 </svelte:head>
 
 <div class="flex m-4">
+	<!-- Current weather -->
 	<div class="glassmorph mr-2 p-2 rounded-2xl">
 		<CurrentWeatherCard />
 	</div>
+
+	<!-- Today's insights -->
 	<div class="grow place-content-center ml-2 glassmorph rounded-2xl">
 		<p class="text-center">Today's Insights</p>
 	</div>
 </div>
+
+<!-- Other cities -->
 <div class="m-4 mt-0 place-content-center glassmorph rounded-2xl grow">
 	<p class="text-center">Other Cities</p>
 </div>
