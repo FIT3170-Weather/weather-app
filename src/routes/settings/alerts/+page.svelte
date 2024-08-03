f3ab49fea77da2f9fd5bc00245e68bb992957934
ryanCYJ <rcho0046@student.monash.edu>
2024-08-03 11:13:00 +0800

    fixed error when loading into profile page

--- src/routes/settings/alerts/+page.svelte
+++ src/routes/settings/alerts/+page.svelte
@@ -64,7 +64,7 @@
 	<meta name="description" content="Climate web app" />
 </svelte:head>
 
-<section class="p-10">
+<div class="p-10">
     <div class="h-max text-4xl font-semibold" style="padding-bottom: 30px;">Alerts</div>
     <div class="flex-grow border-t border-primary-content"></div> 
     <div class="h-max text-2xl font-semibold py-5">Observed Locations</div>
@@ -166,7 +166,7 @@
             </div>
         </div>
     </dialog>
-</section>
+</div>
 
 
 
