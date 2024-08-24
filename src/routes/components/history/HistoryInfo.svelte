d98c2448515e6dfd401a8db11d8b5a6972964f68
nlee0060 <nlee0060@student.monash.edu>
2024-08-24 22:59:53 +0800

    connect more weather description to their respective image

--- src/routes/components/history/HistoryInfo.svelte
+++ src/routes/components/history/HistoryInfo.svelte
@@ -4,20 +4,46 @@
     export let message = "Real Feel";
     export let temp = "29°C";
     import { getWeatherConditionImage } from "../../weatherConditions";
-	import { WeatherConditions } from "../../weatherConditions"; 
+	import { WeatherConditions } from "../../weatherConditions";
+    
+    
+    let clear_sky_description = ["Partly clouds","Mostly clear"];
+    let few_cloud_description = ["Partly clouds","Mostly clear"];
+    let scattered_cloud_description = ["Overcast"];
+    let broken_cloud_description = ["Broken clouds"];
+    let light_rain_description = ["Slight rain shower","Light drizzle","Slight rain","Slight rain","Light freezing drizzle","Light freezing rain"];
+    let moderate_rain_description = ["Moderate rain shower","Moderate drizzle","Moderate rain","Dense freezing drizzle","Heavy freezing rain"];
+    let thunder_storm_description = ["Thunderstorm","Dense drizzle","Heavy rain","Violent rain shower"];
+    let fog_description = ["Fog"];
 
     function getWeatherConditionImages (weather_description: string): any {
-        switch (weather_description) {
-            case "Clear sky": return getWeatherConditionImage(WeatherConditions.CLEAR);
-            case "Partly clouds":return getWeatherConditionImage(WeatherConditions.FEW_CLOUD);
-            case "Overcast": return getWeatherConditionImage(WeatherConditions.SCATTERED_CLOULD);
-            case "Broken clouds":return getWeatherConditionImage(WeatherConditions.BROKEN_CLOUD);
-            case "Slight rain shower": return getWeatherConditionImage(WeatherConditions.LIGHT_RAIN);
-            case "Moderate rain shower": return getWeatherConditionImage(WeatherConditions.MODERATE_RAIN);
-            case "Thunderstorm": return getWeatherConditionImage(WeatherConditions.THUNDER_STORM);
-            case "Fog": return getWeatherConditionImage(WeatherConditions.FOG);
-            default: return getWeatherConditionImage(WeatherConditions.CLEAR);
+        if (clear_sky_description.includes(weather_description)) {
+            return getWeatherConditionImage(WeatherConditions.CLEAR);
         }
+        else if (few_cloud_description.includes(weather_description)) {
+            return getWeatherConditionImage(WeatherConditions.FEW_CLOUD);
+        }
+        else if (scattered_cloud_description.includes(weather_description)) {
+            return getWeatherConditionImage(WeatherConditions.SCATTERED_CLOULD);
+        }
+        else if (broken_cloud_description.includes(weather_description)) {
+            return getWeatherConditionImage(WeatherConditions.BROKEN_CLOUD);
+        }
+        else if (light_rain_description.includes(weather_description)) {
+            return getWeatherConditionImage(WeatherConditions.LIGHT_RAIN);
+        } 
+        else if (moderate_rain_description.includes(weather_description)) {
+            return getWeatherConditionImage(WeatherConditions.MODERATE_RAIN);
+        }  
+        else if (thunder_storm_description.includes(weather_description)) {
+            return getWeatherConditionImage(WeatherConditions.THUNDER_STORM);
+        }
+        else if (fog_description.includes(weather_description)){
+            return getWeatherConditionImage(WeatherConditions.FOG);
+        } 
+        else {
+            return getWeatherConditionImage(WeatherConditions.CLEAR);
+        }      
     }
 </script>
 <div class=" text-white space-y-2 w-full h-full flex flex-col justify-center items-center">
